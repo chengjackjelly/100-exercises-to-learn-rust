0758b21050dc3197b242044263185144e425153e
jiajun chen <jiajun.chen@scania.com>
2024-07-20 15:46:57 +0200

    04_06-09

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -1,7 +1,20 @@
 // TODO: Implement the `From` trait for the `WrappingU32` type to make `example` compile.
 
+
+
 pub struct WrappingU32 {
     value: u32,
+}
+//impl Into<WrappingU32> for u32 {
+    //fn into(self) -> WrappingU32 {
+        //WrappingU32{value: self}
+    //}
+//}
+impl From<u32> for WrappingU32 {
+    fn from(item: u32) -> Self {
+        WrappingU32{value:item}
+    }
+    
 }
 
 fn example() {
